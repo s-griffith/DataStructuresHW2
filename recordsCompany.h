c4c51e893d45f31440019f639d72777c22809f28
Sarah <cmcgriffith@gmail.com>
2023-06-21 12:45:07 +0300

    organized

--- recordsCompany.h
+++ recordsCompany.h
@@ -9,11 +9,24 @@
 #include "GenericNode.h"
 #include "RankNode.h"
 #include "Record.h"
-#include "Stack.h" //MIGHT WANT TO GET RID OF THIS CLASS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+#include "Stack.h" 
 
 
 class RecordsCompany {
   private:
+
+    //The initial size of the hash table
+    static const int INITIAL_HASH = 7;
+
+    /*
+     * The internal fields of RecordsCompany:
+     *   The number of records in the system
+     *   The current size of the hash table
+     *   The number of customers in the system
+     *   An AVL tree of members
+     *   A hash table of AVL trees of customers
+     *   An array of pointers to records
+     */
     int m_numRecords;
     int m_currentHashSize;
     int m_numCustomers;
@@ -21,9 +34,31 @@ class RecordsCompany {
     Tree<GenericNode<Customer*>, Customer*>** m_customersHashTable;
     Record** m_records;
 
+    //-------------------------------------------Helper Functions----------------------------------------------
+
+    /*
+    * When the number of customers = size of table, the table needs to be enlarged to ensure time complexity of O(1)
+    *   for inserting + searching for customers
+    * @return - none
+    */
     void enlarge_hash_table();
+
+    /*
+    * Deallocate memory of previous hash table
+    * @return - none
+    */    
     void destroy_old_hash_table(Tree<GenericNode<Customer*>, Customer*>** tmpTable);
+
+    /*
+    * Insert a new customer to hash table
+    * @return - none
+    */
     void insert_customer_hash_table(Customer* tmpCustomer);
+
+    /*
+    * Calculate placement of customer in hash table according to the hash function
+    * @return - integer that is equal to the customer's index in the hash table
+    */
     int hash_function(int id);
 
   public:
